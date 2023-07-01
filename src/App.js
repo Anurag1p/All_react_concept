154bbd2617738e726dc9976610ad98c532816735
Anuragpal2p <112432540+Anuragpal2p@users.noreply.github.com>
2023-07-01 16:53:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import "./App.css";
-import Axios from "axios";
-import AxiosPost from "./components/AxiosPost";
-import Controlled from "./components/Controlled";
-import UncontrolledCompo from "./components/UncontrolledCompo";
-import AllDefinations from "./components/AllDefinations";
-import Reusablecomponent from "./components/Reusablecomponent";
-import StateInReact from "./components/StateInReact";
-import UseStatehook from "./components/UseStatehook";
 // import UseEffectHook from "./components/UseEffectHook";
 // import TodoList from './TodoList';
 
-function App() {
+import Todo from "./components/Todo";
+
+const App = () => {
   return (
     <>
-      <div className="App">
-     <h1>Hello World</h1>
-      </div>
+     <Todo/>
     </>
   );
-}
+};
 
 export default App;
